77d1d13c41209c9b7108cfb8b37902824de8969b
John <john.milner@mac.com>
2017-11-06 11:56:17 -0800

    set scrubber to zero

--- scripts-FINISHED.js
+++ scripts-FINISHED.js
@@ -37,6 +37,7 @@ function scrub(e) {
   video.currentTime = scrubTime;
 }
 
+
 /* Hook up the event listners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
